dedcff64742a4711ec5d7d8b7088aedf8750ef6a
TaniaWinters <S00213820@mail.itsligo.ie>
2022-08-29 23:23:47 +0100

    Project
    Web Project - improvements

---
+++ js/products.js
@@ -0,0 +1,39 @@
+// PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Hair Spray",
+    desc : "Strong Hold spray for upstying and blowdrys. ",
+    img : "hairspray.jpeg",
+    price : 20.00
+  },
+  124: {
+    name : "Gift Set Brushes",
+    desc : "A handy set of blowdrying brushes for all types of hair.",
+    img : "giftset.jpg",
+    price : 25.00
+  },
+  125: {
+    name : "Clarifying Shampoo",
+    desc : "A Shampoo which clears product/sea salt out of the hair by bringing it back to it natural state.",
+    img : "shampooClarify.jpeg",
+    price : 36.50
+  },
+  126: {
+    name : "Kemon Conditioner",
+    desc : "A lovely mask to condition any time of hair leaving it feeling soft. ",
+    img : "kemonConditioner.jpeg",
+    price : 42.50
+  },
+   127: {
+    name : "Kemon Purezza",
+    desc : "A Shampoo to help smooth psoriasis and scalp sensitivity.",
+    img : "KemonPurezza.jpeg",
+    price : 50.50
+  },
+  128: {
+    name : "Wand Curler",
+    desc : "A wand curler which is easier to use while styling your own hair with elegant curls. ",
+    img : "curler.jpeg",
+    price : 70.99
+  }
+};
